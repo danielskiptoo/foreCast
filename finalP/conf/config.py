f1d40614db565f1ee9f1e6177f56ce829a32ed54
danielskiptoo <kiptood6@gmail.com>
2018-04-03 09:59:50 +0300

    update

--- finalP/conf/config.py
+++ finalP/conf/config.py
@@ -6,6 +6,9 @@ DB_HOST= '127.0.0.1'
 DB_PORT= '5432'
 
 
+ALLOWED_HOSTS = ['forecas.herokuapp.com','127.0.0.1']
+
+
 SECRET_KEY = 'rz&mw1v(5*-g8c0-z5j40y6(imdzxln19c*_mu8v%_j-&2y_q@'
 
 EMAIL_FILE_PATH = '/tmp/app-messages'
