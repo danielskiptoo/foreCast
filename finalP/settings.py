2b022b467d21681a05562ecf0bbce71fc0e9256a
danielskiptoo <kiptood6@gmail.com>
2018-04-03 09:24:32 +0300

    update

--- finalP/settings.py
+++ finalP/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 SITE_ID =1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
